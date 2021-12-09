e1751a224ee01c3bb60933637f4fc5503bfd6ac0
M0hammed2001 <92979865+M0hammed2001@users.noreply.github.com>
2021-12-09 21:26:44 +0000

    v
    new version has options and leaderboards not working yet.

---
+++ Final/Assets/Scripts/Options.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrutalCards
+
+public class Options : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
